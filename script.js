c38e15084f418e2694360a5d227a8de117eb96c2
AnnaZietarska <anna.zietarska@gmail.com>
2021-06-18 09:41:12 +0200

    Created the CSS and JS files

---
+++ script.js
@@ -0,0 +1,46 @@
+function currentDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  return `${days[day]}, ${hours}:${minutes}`;
+}
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+
+  let apiKey = "8f761dae0535ae2100daebcd1f879514";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let dateElement = document.querySelector("#date");
+let now = new Date();
+dateElement.innerHTML = currentDate(now);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
