2efbda3687bb396cbd8e1547c1af54c155fc0fd9
AnnaZietarska <anna.zietarska@gmail.com>
2021-07-16 10:26:19 +0200

    changed color

--- script.js
+++ script.js
@@ -61,7 +61,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "8f761dae0535ae2100daebcd1f879514";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
